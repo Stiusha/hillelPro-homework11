9db02b6fc77b295acf171d5083db793601e1a64f
stiusha <stiusha0110@gmail.com>
2026-05-13 20:54:34 +0300

    HM11 refactoring

--- src/app/FileHandler.java
+++ src/app/FileHandler.java
@@ -1,38 +1,32 @@
 package app;
 
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class FileHandler {
 
-    public String createFile(String path) {
-        Path newFile;
+    public void createFile(String path) {
         try {
-            newFile = Files.createFile(Path.of(path));
-        } catch (FileAlreadyExistsException e) {
-            return "File already exists!";
+            Files.createFile(Path.of(path));
         } catch (IOException e) {
-            return "Something wrong " + e.getMessage();
+            throw new FileProcessingException(e);
         }
-        return "Created " + newFile;
     }
 
-    public String writeToFile(String path, String content) {
+    public void writeToFile(String path, String content) {
         try {
             Files.writeString(Path.of(path), content);
         } catch (IOException e) {
-            return e.getMessage();
+            throw new FileProcessingException(e);
         }
-        return "Recorded in " + path;
     }
 
     public String readFromFile(String path) {
         try {
             return Files.readString(Path.of(path));
         } catch (IOException e) {
-            return "Something wrong " + e.getMessage();
+            throw new FileProcessingException(e);
         }
     }
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -7,12 +7,19 @@ public class Main {
     public static void main(String[] args) {
         FileHandler handler = new FileHandler();
 
-        String newFileName = "myfile";
+        String newFileName = "myfile.txt";
         String content = "Super information.";
-        String path = BASE_PATH + newFileName + ".txt";
+        String path = BASE_PATH + newFileName;
 
-        System.out.println(handler.createFile(path));
-        System.out.println(handler.writeToFile(path, content));
-        System.out.println("CONTENT: " + handler.readFromFile(path));
+        try {
+            handler.createFile(path);
+            System.out.println("Created " + path);
+            handler.writeToFile(path, content);
+            System.out.println("Recorded in " + path);
+            String readFromFile = handler.readFromFile(path);
+            System.out.println("CONTENT: " + readFromFile);
+        } catch (FileProcessingException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
